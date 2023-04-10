653c01dcda5860e20a0b584f9cd65820f2fe7d25
vanxh <vanxhyt@gmail.com>
2023-04-10 13:23:30 +0530

    app build page

--- apps/web/src/components/AppEdit/AppBuild.tsx
+++ apps/web/src/components/AppEdit/AppBuild.tsx
@@ -1,5 +1,18 @@
+import { Delete, File, Trash } from "lucide-react";
 import { useState } from "react";
-import { Input, Label, Button, RadioGroup, RadioGroupItem } from "ui";
+import {
+  Input,
+  Textarea,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Label,
+  Button,
+  RadioGroup,
+  RadioGroupItem,
+} from "ui";
 
 const AppBuildRow = ({
   children,
@@ -32,6 +45,8 @@ export default function AppBuild() {
   const isAndroid = appType === "android";
   const isWeb = appType === "web";
   const isIOS = appType === "ios";
+
+  const [apk, setApk] = useState<File[]>([]);
 
   return (
     <div className="flex flex-col items-center justify-start w-full rounded-lg bg-white shadow-[0_20_20_60_#0000000D] overflow-hidden">
@@ -79,7 +94,116 @@ export default function AppBuild() {
 
         <hr />
 
-        {isAndroid && <AppBuildRow label="Android">TODO</AppBuildRow>}
+        {isAndroid && (
+          <AppBuildRow label="Android">
+            <div className="flex items-center justify-center w-full">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    aria-hidden="true"
+                    className="w-10 h-10 mb-3 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    ></path>
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                  </p>
+                </div>
+                <input
+                  id="dropzone-file"
+                  type="file"
+                  // accept=".apk"
+                  className="hidden"
+                  onChange={(e) => {
+                    if (!e.target.files) return;
+                    setApk(Array.prototype.slice.call(e.target.files));
+                  }}
+                />
+              </label>
+            </div>
+            {apk.map((a) => (
+              <div
+                key={a.name}
+                className="w-full h-full justify-center border border-[#2678FD] rounded-lg p-4 flex flex-col gap-y-3"
+              >
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex flex-row gap-x-2 w-[80%]">
+                    <div className="w-8 h-8 bg-[#EDF4FF] rounded-full flex items-center justify-center">
+                      <File className="w-4 h-4 text-[#2678FD]" />
+                    </div>
+
+                    <div className="flex flex-col gap-y-1 w-[70%]">
+                      <p className="font-medium text-sm truncate">{a.name}</p>
+                      <p className="text-sm text-[#475467]">
+                        {(a.size / 1024 / 1024).toFixed(2)}
+                        MB
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => {
+                      setApk((prev) => prev.filter((p) => p.name !== a.name));
+                    }}
+                    className="ease-in-out transition-all active:scale-90"
+                  >
+                    <Trash className="h-4 w-4 text-[#667085]" />
+                  </button>
+                </div>
+              </div>
+            ))}
+
+            <br />
+
+            <div className="flex flex-row gap-x-2 w-full">
+              <div className="flex flex-col gap-y-2 w-[50%]">
+                <Label>Architecture</Label>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select architecture" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">All</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-y-2 w-[50%]">
+                <Label>Screen DPI</Label>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select screen DPI" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">All</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>
+                Minimum Version
+                <span className="text-red-500">*</span>
+              </Label>
+              <Input placeholder="version" />
+            </div>
+          </AppBuildRow>
+        )}
 
         {isWeb && <AppBuildRow label="Web">TODO</AppBuildRow>}
 
